063730b40bc62f27c99d2c8447a2208e0c9c1a94
unknown <apurba67141@gmail.com>
2023-06-24 22:09:03 +0530

    navber done

--- src/components/page/Shared/Navbar.jsx
+++ src/components/page/Shared/Navbar.jsx
@@ -1,10 +1,36 @@
-
+import logo from '../../../assets/image/logo.png';
 
 const Navbar = () => {
+  const menuitems = <>
+        <li><a>Home</a></li>
+        <li><a>Track Order</a></li>
+        <li><a>Brand</a></li>
+        <li><a>Shop</a></li>
+        <li><a>Services</a></li>
+        <li><a>Contact Us</a></li>
+  </>
   return (
-    <div>
-      <h1>this is nave bar</h1>
+    <div className="navbar bg-base-100">
+    <div className="navbar-start">
+      <div className="dropdown">
+        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        </label>
+        <ul tabIndex={0} className=" z-50 menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          {menuitems}
+        </ul>
+      </div>
+      <a className="btn btn-ghost normal-case text-xl"><img src={logo} alt=''></img></a>
     </div>
+    <div className="navbar-center hidden lg:flex">
+      <ul className="menu z-50 menu-horizontal px-1">
+        {menuitems}
+      </ul>
+    </div>
+    <div className="navbar-end">
+      <a className="btn">Button</a>
+    </div>
+  </div>
   );
 };
 
